b0e5f80bdeca04b84b95a071aa03aa1925b0811b
diegoejp <d.jacobpulgar@gmail.com>
2021-06-09 09:41:06 -0400

    semaforo perfeccionado

--- src/js/component/Semaforo.js
+++ src/js/component/Semaforo.js
@@ -2,40 +2,25 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const Semaforo = props => {
-	const aux = { boxShadow: "0 0 100px #4f9" };
-	const [up, setUp] = useState(null);
-	const [middle, setMiddle] = useState(null);
-	const [down, setDown] = useState(null);
-
-	function apretarA() {
-		setUp(aux);
-		setMiddle(null);
-		setDown(null);
-	}
-	function apretarB() {
-		setUp(null);
-		setMiddle(aux);
-		setDown(null);
-	}
-	function apretarC() {
-		setUp(null);
-		setMiddle(null);
-		setDown(aux);
-	}
+	const [selected, setSelected] = useState("red");
 
 	return (
 		<>
 			<div className="palo"></div>
 			<div className="cuerpo">
-				<div className="luz rojo " style={up} onClick={apretarA}></div>
 				<div
-					className="luz amarillo"
-					style={middle}
-					onClick={apretarB}></div>
+					className={"luz rojo " + (selected === "red" ? "on" : "")}
+					onClick={() => setSelected("red")}></div>
 				<div
-					className="luz verde"
-					style={down}
-					onClick={apretarC}></div>
+					className={
+						"luz amarillo " + (selected === "amarillo" ? "on" : "")
+					}
+					onClick={() => setSelected("amarillo")}></div>
+				<div
+					className={
+						"luz verde " + (selected === "verde" ? "on" : "")
+					}
+					onClick={() => setSelected("verde")}></div>
 			</div>
 		</>
 	);
